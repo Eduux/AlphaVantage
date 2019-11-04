a838ac4e1b3e76a80176b1976b384d95077a4138
Squadros <eduardo.quadros@southsystem.com.br>
2019-11-04 11:44:37 -0300

    Updates

---
+++ precache-manifest.314e1055946267bd6b52589d8e443c8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d6f06a6273b181878e8d895f8fec58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdeeb1e7d896900d9a62",
+    "url": "/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "cdeeb1e7d896900d9a62",
+    "url": "/static/js/2.003a0fe2.chunk.js"
+  },
+  {
+    "revision": "75dc344b23e84ef6464b",
+    "url": "/static/js/main.d9ef7894.chunk.js"
+  },
+  {
+    "revision": "616e55cadfccd470dfb0",
+    "url": "/static/js/runtime-main.c757f4a9.js"
+  },
+  {
+    "revision": "f2fc0a55567ed9e0f1343b715ff5ea22",
+    "url": "/static/media/bank.f2fc0a55.svg"
+  },
+  {
+    "revision": "009e7e12fe1583c7e0b02b7b4b87df50",
+    "url": "/static/media/error-404.009e7e12.svg"
+  }
+]);
